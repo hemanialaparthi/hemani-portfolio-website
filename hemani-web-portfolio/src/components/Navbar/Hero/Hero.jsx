b934179e90c75e21028ce3cb32f7b191f956117c
hemanialaparthi <alaparthi01@allegheny.edu>
2024-07-25 14:01:11 -0400

    complete nav

---
+++ hemani-web-portfolio/src/components/Navbar/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+  return <section>
+    <div>
+        <h1>Hi I am Hemani</h1>
+        <p> I am a sophmore who is a double major in compsci & Econ and I am aspiring full stack developer </p>
+        <a href="mailto:hemanialaparthi@gmail.com">Contact Me!</a>
+    </div>
+    <img src=''></img>
+  </section>
+}
+
+export default Hero
