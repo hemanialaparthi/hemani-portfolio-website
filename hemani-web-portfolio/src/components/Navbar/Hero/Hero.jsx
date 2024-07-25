c7b98a6010c2a1cd634529eb64945a24a0f2e18d
hemanialaparthi <alaparthi01@allegheny.edu>
2024-07-25 14:44:11 -0400

    finish Hero

--- hemani-web-portfolio/src/components/Navbar/Hero/Hero.jsx
+++ hemani-web-portfolio/src/components/Navbar/Hero/Hero.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import { getImageUrl } from '../../../utils'
+import styles from "./Hero.module.css"
 
 const Hero = () => {
-  return <section>
-    <div>
-        <h1>Hi I am Hemani</h1>
-        <p> I am a sophmore who is a double major in compsci & Econ and I am aspiring full stack developer </p>
-        <a href="mailto:hemanialaparthi@gmail.com">Contact Me!</a>
+  return <section className={styles.container}>
+    <div className={styles.content}>
+        <h1 className={styles.title}>Hi I am Hemani</h1>
+        <p className={styles.description}> I am a sophmore who is a double major in compsci & Econ and I am aspiring full stack developer </p>
+        <a href="mailto:hemanialaparthi@gmail.com" className={styles.contactBtn}>Contact Me!</a>
     </div>
-    <img src=''></img>
+    <img src={getImageUrl("hero/pfp.png")} alt="profile picure" className={styles.heroImg}/>
+        {/* highlight 2 of the pages */}
+        <div className={styles.topBlur}/>
+        <div className={styles.bottomBlur}/>
   </section>
 }
 
