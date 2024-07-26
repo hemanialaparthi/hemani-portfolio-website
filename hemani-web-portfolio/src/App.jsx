967264047d3a516fa3ccdd94c1f907989a11a230
hemanialaparthi <alaparthi01@allegheny.edu>
2024-07-26 11:23:17 -0400

    edit: image size

--- hemani-web-portfolio/src/App.jsx
+++ hemani-web-portfolio/src/App.jsx
@@ -2,8 +2,8 @@
 import styles from './App.module.css'
 import Hero from './components/Hero/Hero'
 import { Navbar } from './components/Navbar/Navbar'
-import About from './components/About/About'
-import Skills from './components/Skills/Skills'
+import { About } from './components/About/About'
+import { Skills } from './components/Skills/Skills'
 
 function App() {
 
--- hemani-web-portfolio/src/components/Skills/Skills.jsx
+++ hemani-web-portfolio/src/components/Skills/Skills.jsx
@@ -1,7 +1,41 @@
 import React from 'react'
+import skills from '../../data/skills.json'
+import { getImageUrl } from "../../utils"
+import education from "../../data/education.json"
+import styles from "./Skills.module.css"
 
 export const Skills = () => {
-  return <section>
-    
-  </section>
+    return <section className={styles.container} id="Skills">
+        <h2 className={styles.title}>Education</h2>
+        <div className={styles.content}>
+            <div className={styles.skills}>{
+                skills.map((skill, id) => {
+                    return <div key={id} className={styles.skills}>
+                        <div className={styles.skillImageContainer}>
+                            <img src={getImageUrl(skill.imageSrc)} alt={skill.title}></img>
+                        </div>
+                        <p>{skill.title}</p>
+                    </div>
+                })
+                }</div>
+            <ul className={styles.education}>
+                {
+                    education.map((educationItem, id) => {
+                        return <li key={id} className={styles.educationItem}>
+                            <img src={getImageUrl(educationItem.imageSrc)} alt={`${educationItem.school} Logo`}/>
+                            <div className={styles.educationItemDetails}>
+                                <h3>
+                                    {`${educationItem.school}, ${educationItem.major}`}
+                                </h3>
+                                <p>{`${educationItem.startDate} - ${educationItem.endDate}`}</p>
+                                <ul>{educationItem.extracurs.map((extracur, id) => {
+                                    return <li key={id}>{extracur}</li>
+                                })}</ul>
+                            </div>
+                        </li>
+                    })
+                }
+            </ul>
+        </div>
+    </section>;
 }
