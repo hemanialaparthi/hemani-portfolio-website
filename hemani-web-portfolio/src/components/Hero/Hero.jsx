f106963176685e49729540d504ba468f31137e7b
hemanialaparthi <alaparthi01@allegheny.edu>
2024-07-27 16:31:35 -0400

    add type animation!

--- hemani-web-portfolio/src/components/Hero/Hero.jsx
+++ hemani-web-portfolio/src/components/Hero/Hero.jsx
@@ -1,19 +1,39 @@
-import React from 'react'
-import { getImageUrl } from '../../utils'
-import styles from "./Hero.module.css"
+import React from 'react';
+import { getImageUrl } from '../../utils';
+import styles from "./Hero.module.css";
+import { TypeAnimation } from 'react-type-animation';
 
 const Hero = () => {
-  return <section className={styles.container}>
-    <div className={styles.content}>
-        <h1 className={styles.title}>Hi I am Hemani</h1>
-        <p className={styles.description}> I am a sophmore who is a double major in compsci & Econ and I am aspiring full stack developer </p>
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>
+          Hi, I'm {""}
+          <TypeAnimation
+            sequence={[
+              "Hemani.",
+              2000, // pause for 2 seconds
+              "a Student.",
+              2000,
+              "a Full Stack Developer.",
+              2000,
+            ]}
+            wrapper="span"
+            speed={50}
+            repeat={Infinity}
+            className={styles.rolesAnimation}
+          />
+        </h1>
+        <p className={styles.description}>
+          Rising sophomore @ Allegheny College. Double majoring in Computer Science & Economics.
+        </p>
         <a href="mailto:hemanialaparthi@gmail.com" className={styles.contactBtn}>Contact Me!</a>
-    </div>
-    <img src={getImageUrl("hero/pfp.png")} alt="profile picure" className={styles.heroImg}/>
-        {/* highlight 2 of the pages */}
-        <div className={styles.topBlur}/>
-        <div className={styles.bottomBlur}/>
-  </section>
-}
+      </div>
+      <img src={getImageUrl("hero/pfp.png")} alt="profile picture" className={styles.heroImg}/>
+      <div className={styles.topBlur}/>
+      <div className={styles.bottomBlur}/>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
