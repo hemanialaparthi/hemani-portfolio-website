df8fd5f4a3a63a744e5635a919d43bf7f563d2a7
hemanialaparthi <alaparthi01@allegheny.edu>
2024-07-27 15:54:53 -0400

    complete contact

--- hemani-web-portfolio/src/App.jsx
+++ hemani-web-portfolio/src/App.jsx
@@ -5,6 +5,7 @@ import { Navbar } from './components/Navbar/Navbar'
 import { About } from './components/About/About'
 import { Skills } from './components/Skills/Skills'
 import { Projects } from './components/Projects/Projects'
+import { Contact } from './components/Contact/Contact'
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
       <About />
       <Skills />
       <Projects />
+      <Contact />
     </div>
   )
 }
